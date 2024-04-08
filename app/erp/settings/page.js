7969caeef454eafedbb06eb56f743547c1284f41
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-04-08 00:49:20 -0700

    working on erp-settings

---
+++ app/erp/settings/page.js
@@ -0,0 +1,7 @@
+import Settings from "@/components/erp/settings";
+
+const Sett = ()=>{
+    return <Settings />
+}
+
+export default Sett;
---
+++ components/erp/settings/index.jsx
@@ -0,0 +1,13 @@
+import ErpLayout from "@/components/shared/layout/erp";
+
+const Settings = ()=>{
+    return (
+        <ErpLayout>
+            <div>
+               erp settings
+            </div>
+        </ErpLayout>
+    )
+}
+
+export default Settings;
